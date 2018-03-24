11be24d92495068ccf2b5a9d002793e41a5ed98c
MienAnh <anhmiennguyen96@gmail.com>
2018-03-24 21:51:14 +0700

    Hien thi Danh Muc

--- ShopBooks/ShopBooks/Controllers/HomeController.cs
+++ ShopBooks/ShopBooks/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopBooks.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,24 @@ namespace ShopBooks.Controllers
 {
     public class HomeController : Controller
     {
+        ModelDbContext db = new ModelDbContext();
         // GET: Home
         public ActionResult Index()
+        {
+            List<ChuDe> ls = new List<ChuDe>();
+            foreach(var item in db.ChuDes)
+            {
+                ls.Add(item);
+            }
+            return View(ls);
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult About()
         {
             return View();
         }
