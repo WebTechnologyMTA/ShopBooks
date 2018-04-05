5f09a2a234396a31cd800ddeb0c11b5668cbd53b
MienAnh <anhmiennguyen96@gmail.com>
2018-04-05 21:31:36 +0700

    Update

--- ShopBooks/ShopBooks/Controllers/HomeController.cs
+++ ShopBooks/ShopBooks/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace ShopBooks.Controllers
         public ActionResult Index()
         {
             List<Sach> ls = new List<Sach>();
-            var ListProduct = db.Saches.Take(10);
+            var ListProduct = db.Saches.Take(3);
             foreach (var item in ListProduct)
             {
                 item.AnhBia = "/Content/assets/product_img/" + item.AnhBia;
@@ -32,7 +32,7 @@ namespace ShopBooks.Controllers
         {
             return View();
         }
-
+        // GET: Chu De
         public PartialViewResult PartialMenuList()
         {
             List<ChuDe> ls = new List<ChuDe>();
@@ -41,6 +41,26 @@ namespace ShopBooks.Controllers
                 ls.Add(item);
             }
             return PartialView("_PartialMenuList", ls);
+        }
+
+        // GET: Category/ID
+        public ActionResult Category(int ID)
+        {
+            List<Sach> ls = db.Saches.Where(x => x.MaChuDe == ID).ToList();
+            foreach(var item in ls)
+            {
+                item.AnhBia = "/Content/assets/product_img/" + item.AnhBia;
+            }
+            return View(ls);
+        }
+
+        // GET: ProductDetail/ID
+        public ActionResult ProductDetail(int ID)
+        {
+            var sach = db.Saches.Where(x => x.MaSach == ID).SingleOrDefault();
+            sach.AnhBia = "/Content/assets/product_img/" + sach.AnhBia;
+            sach.NhaXuatBan = db.NhaXuatBans.Where(x => x.MaNXB == sach.MaNXB).SingleOrDefault();
+            return View(sach);
         }
     }
 }
--- ShopBooks/ShopBooks/Controllers/ProductController.cs
+++ ShopBooks/ShopBooks/Controllers/ProductController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopBooks.Models;
 
 namespace ShopBooks.Controllers
 {
     public class ProductController : Controller
     {
+        ModelDbContext db = new ModelDbContext();
         // GET: Product
-        public ActionResult ProductDetails()
+        public ActionResult Category(int ID)
         {
-            return View();
+            var chude = db.ChuDes.Where(x => x.MaChuDe == ID);
+            return View(chude);
+        }
+
+        // GET: ProductDetail/ID
+        public ActionResult ProductDetail(int ID)
+        {
+            var sach = db.Saches.Where(x => x.MaSach == ID).SingleOrDefault();
+            sach.AnhBia = "/Content/assets/product_img/" + sach.AnhBia;
+            sach.NhaXuatBan = db.NhaXuatBans.Where(x => x.MaNXB == sach.MaNXB).SingleOrDefault();
+            return View(sach);
         }
     }
 }
